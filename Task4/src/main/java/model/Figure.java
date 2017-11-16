816ad5e176dfe83f83cbc835895b498fd74f353a
NikaStark <k7a3r5@gmail.com>
2017-11-16 12:58:20 +0200

    Create class Figure.java and interface Colorable.java

---
+++ Task4/src/main/java/model/Figure.java
@@ -0,0 +1,19 @@
+package model;
+
+public abstract class Figure {
+
+    @Override
+    public String toString() {
+        return "Figure{" + toStringParameters() + "}";
+    }
+
+    /**
+     * Outputs this vehicle in short form(without name class) as a {@code String}.
+     *
+     * @return a string representation of this year, not null
+     */
+    protected String toStringParameters() {
+        return  "";
+    }
+
+}
---
+++ Task4/src/main/java/model/interfaces/Colorable.java
@@ -0,0 +1,12 @@
+package model.interfaces;
+
+/**
+ * Interface for color figure
+ *
+ * @author Alex Volochai
+ */
+public interface Colorable {
+
+    int getColor();
+
+}
