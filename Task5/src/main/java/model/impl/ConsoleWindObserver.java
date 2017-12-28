09ce2e76ef2832784dd9399618d16004994b6598
NikaStark <k7a3r5@gmail.com>
2017-12-29 00:27:23 +0200

    Refactor pom.xml and ConsoleWindObserver

--- Task5/src/main/java/model/impl/ConsoleWindObserver.java
+++ Task5/src/main/java/model/impl/ConsoleWindObserver.java
@@ -4,6 +4,9 @@ import model.Observable;
 import model.Observer;
 import view.ConsolePrint;
 
+/**
+ * @author Alex Volodhai
+ */
 public class ConsoleWindObserver implements Observer {
 
     @Override
